1356b710db91535834ffa444f49ad37103ed1cdf
alexanderajju <alexanderajju@gamil.com>
2020-06-27 11:31:58 +0530

    initial

---
+++ calculator.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+window=Tk()
+window.geometry("354x460")
+window.title("Calculator")
+label = Label(window,text="Calculator",font=("Courier New",30,'bold'))
+window.mainloop()
